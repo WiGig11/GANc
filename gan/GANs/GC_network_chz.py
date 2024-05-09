cfa5ad00c796b6ef02032d8003790fd5810fb280
WiGig11 <982155691@qq.com>
2024-05-09 09:39:04 +0800

    update chz

--- gan/GANs/GC_network_chz.py
+++ gan/GANs/GC_network_chz.py
@@ -8,24 +8,44 @@ import numpy as np
 from tensorflow_addons.layers import InstanceNormalization
 import time
 import pdb
+import os
 
-raw_image_dataset = tf.data.TFRecordDataset('k19.tfrecords')
-# Create a dictionary describing the features.
-image_feature_description = {
-    'image': tf.io.FixedLenFeature([], tf.string),
-}
+logdir = './tensorboard'
+dirs = os.listdir(logdir)
+max = 0
 
-def _parse_image_function(example_proto):
-   # Parse the input tf.Example proto using the dictionary above.
-    dataset=tf.io.parse_single_example(example_proto, image_feature_description)
-    image_png=tf.io.decode_png(dataset['image'])
-    image_png=tf.image.convert_image_dtype(image_png,dtype=tf.float32)
-    image_png=tf.image.resize(image_png,[256,384])
-    dataset['image']=2*image_png-1
-    return dataset
+for i in range(len(dirs)):
+    name = dirs[i]
+    num = int(name.split('s')[-1])
+    if num>max:
+        max = num
+newlogdir = 'runs'+str(max)
 
-parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
-parsed_image_dataset=parsed_image_dataset.shuffle(8).batch(4)
+summary_writer = tf.summary.create_file_writer(os.path.join(logdir,newlogdir))     # 参数为记录文件所保存的目录
+
+# 设置图像文件的路径（假设图像都在这个文件夹下）
+image_dir = '/chz/kodak/'
+
+# 创建一个数据集，其中包含图像文件的路径
+list_ds = tf.data.Dataset.list_files(image_dir + '*.png', shuffle=False)
+
+def process_image(file_path):
+    # 读取图像文件
+    img = tf.io.read_file(file_path)
+    img = tf.io.decode_png(img, channels=3)  # 假设是RGB图像
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    #img = tf.image.resize(img, [256, 384])
+    img = 2 * img - 1  # 归一化处理
+    return img
+
+# 映射处理函数，加载和预处理图像
+images_ds = list_ds.map(process_image)
+
+# 批处理和洗牌
+batch_size = 1
+EPOCH=1026
+
+train_ds = images_ds.shuffle(24).batch(batch_size)
 
 BottleNeck=8
 
@@ -46,7 +66,7 @@ def mypad(x,size):
 
 def encoderGC_model(C=BottleNeck):
     #print("encoderGC is called")
-    input=layers.Input(shape=(256,384,3))
+    input=layers.Input(shape=(None,None,3))
     #print("input_shape=",input.shape)
     x=layers.Lambda(mypad,arguments={'size':3})(input)
 
@@ -113,7 +133,7 @@ def generator_model(C=BottleNeck):
         #print("upsblk return")
         return model
 
-    input=layers.Input(shape=(16,24,C))
+    input=layers.Input(shape=(None,None,C))
     
     x=layers.Lambda(mypad,arguments={'size':1})(input)
 
@@ -143,11 +163,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -158,11 +173,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -173,11 +183,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -189,11 +194,6 @@ def generator_model(C=BottleNeck):
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
     #pdb.set_trace()
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -204,11 +204,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -219,11 +214,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -234,11 +224,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -249,11 +234,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -264,11 +244,6 @@ def generator_model(C=BottleNeck):
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
     res = layers.ReLU()(res)
-    fac = layers.GlobalAveragePooling2D()(res)
-    fac = layers.Dense(960//16, activation='relu')(fac)  # Assuming a matching size
-    fac = layers.Dense(960, activation='sigmoid')(fac)
-    fac = layers.Reshape((1, 1, 960))(fac) 
-    res = layers.Multiply()([res, fac]) 
     res=layers.Lambda(mypad,arguments={'size':1})(res)
     res=layers.Conv2D(960, kernel_size=[3,3], strides=[1,1])(res)
     res=InstanceNormalization()(res)
@@ -314,7 +289,7 @@ def conv_block(x, filters, kernel_size=(3,3), strides=2, padding='same'):
 def multiscale_discriminator_model():
     #print("mtidisc is called")
 
-    x=layers.Input(shape=(256,384,3))
+    x=layers.Input(shape=(None,None,3))
     x2=layers.AveragePooling2D(pool_size=(3,3),strides=(2,2),padding='same')(x)
     x4=layers.AveragePooling2D(pool_size=(3,3),strides=(2,2),padding='same')(x2)
     
@@ -357,7 +332,7 @@ def discriminator_loss(real_loss,real_loss2,real_loss4,fake_loss,fake_loss2,fake
     #print("discloss return")
     return D_loss
 
-def train_step(origin_image):
+def train_step(origin_image,batch):
     #print("train_step is called")
     with tf.GradientTape() as gen_tape,tf.GradientTape() as disc_tape:
         w=encoderGC(origin_image)
@@ -374,7 +349,9 @@ def train_step(origin_image):
 
         disc_loss=discriminator_loss(real_output,real2_output,real4_output,fake_output,fake2_output,fake4_output)
         gen_loss=generator_loss(fake_output)+12 * tf.keras.losses.MeanSquaredError()(origin_image, generated_images)
-
+        with summary_writer.as_default():                           
+            tf.summary.scalar("disc_loss", disc_loss,step = batch)
+            tf.summary.scalar("gen_loss", gen_loss,step = batch)
     gradient_of_generator=gen_tape.gradient(gen_loss,generator.trainable_variables)
     gradient_of_discriminator=disc_tape.gradient(disc_loss,discriminator.trainable_variables)
 
@@ -382,38 +359,33 @@ def train_step(origin_image):
     disciminator_optimizer.apply_gradients(zip(gradient_of_discriminator,discriminator.trainable_variables))
     #print("train_step return")
 
-EPOCH=1026*3
+EPOCH=1026
 
-
-
-def save_img(model,image,epoch,index):
+def save_img(model,image,epoch,batch):
     gen_imgs=model(image,training=False)
     for i in range(gen_imgs.shape[0]):
         image=(gen_imgs[i,:,:,:]+1)/2
         image=tf.math.multiply(image,255)
         image=tf.cast(image,dtype=tf.uint8)
         image=tf.image.encode_png(image)
-        with tf.io.gfile.GFile('../Semcom/GAN/chztestk19/C{:02d}_image_{:02d}_at_epoch_{:04d}_kodak.png'.format(BottleNeck,index,epoch), 'wb') as file:
+        with tf.io.gfile.GFile('/chz/res/Semcom/chztestkodak_2/imgs/C{:02d}_image_{:02d}_at_epoch_{:04d}_kodak.png'.format(BottleNeck,(epoch-1)*24+batch,epoch), 'wb') as file:
             file.write(image.numpy())
         print('image_at_epoch_{:04d} have saved'.format(epoch))
 
 
-def train(dataset,epochs):
-    print("train begin")
+def train(dataset, epochs):
+    print("Train begins")
     for epoch in range(epochs):
-        count=0
-        for example in dataset:
-            #print("epoch=",epoch+1)
-            image=example["image"].numpy()
-            train_step(image)
-            count+=1
-            if epoch%25==0:
-                w=encoderGC(example["image"])
-                w_hat=quantizier(w)
-                save_img(generator,w_hat,epoch+1,count)
-    print("train finished")
+        for batch,image in enumerate(dataset):
+            #pdb.set_trace()
+            train_step(image,epoch*24+batch)
+            if epoch % 25 == 0:
+                w = encoderGC(image)
+                w_hat = quantizier(w)
+                save_img(generator, w_hat, epoch + 1,batch)
+    print("Train finished")
 
-train(parsed_image_dataset,EPOCH)
+train(train_ds,EPOCH)
 
 def quantizier_aftertrain(w,L=5):
     #print("quantizier is called")
@@ -422,14 +394,14 @@ def quantizier_aftertrain(w,L=5):
     w_hard = tf.cast(tf.argmin(tf.abs(w_stack - centers), axis=-1), tf.float32) + tf.reduce_min(centers)
     return w_hard
 
-model_dir='../Semcom/GAN/chztestk19'
+model_dir='/chz/res/Semcom/chztestkodak_2'
 
-input_img=layers.Input(shape=(256,384,3))
+input_img=layers.Input(shape=(None,None,3))
 x=input_img
 for i in range(len(encoderGC.layers)):
     if not isinstance(encoderGC.layers[i],layers.InputLayer):
       x=encoderGC.layers[i](x)
-w_hat=layers.Lambda(quantizier_aftertrain,output_shape=(16, 24, 8))(x)
+w_hat=layers.Lambda(quantizier_aftertrain,output_shape=(None,None, 8))(x)
 GC_encoder=tf.keras.Model(inputs=input_img,outputs=w_hat,name="encoder_quant")
 
 import os
@@ -441,7 +413,7 @@ GC_encoder.summary()
 generator.save(os.path.join(model_dir, 'GC_generator_keras_test'))
 generator.summary()
 
-input_img=layers.Input(shape=(256,384,3))
+input_img=layers.Input(shape=(None,None,3))
 x=input_img
 for i in range(len(GC_encoder.layers)):
     if not isinstance(GC_encoder.layers[i],layers.InputLayer):
